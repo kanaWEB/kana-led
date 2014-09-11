d41dc557f2abda4053074f1ba02054fd4f02182e
madnerds <maditnerd@gmail.com>
2014-09-11 17:35:48 +0200

    Add translation

--- language/fr/fr.php
+++ language/fr/fr.php
@@ -1,4 +1,8 @@
 <?php
 $lang["Led"] = "DEL";
 $lang["Speed in ms"] = "Vitesse en ms";
+$lang["Fade"] = "Faire un fondu avec";
+$lang["Turn on"] = "Allumer";
+$lang["Shut off"] = "Eteindre";
+$lang["Blink"] = "Faire clignoter";
 ?>
