64f91a0558f48a8656a3f638af5377c90e0dc962
maditnerd <sarrailh.remi@gmail.com>
2015-07-16 14:59:22 +0200

    Adapt to piget

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,7 @@
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $DIR
-cd ..
-cd actions
-while IFS= read -r -d $'\0' file; do
-	file=$(echo "$file" |sed s/\.[^\.]*$//)
-	if [ -f $file ]
-		then
-			echo "$file"
-			chown root:www-data $file
-			chmod +sx $file
-	fi
-done < <(find /var/www -name *.cpp -type f -print0)
+#!/bin/bash
+/opt/piget/core/install/extendPath
+downloadModule led
+. extendPath
+nginxAuthorize /opt/piget/led/bin/onoff
+nginxAuthorize /opt/piget/led/bin/blink
+nginxAuthorize /opt/piget/led/bin/pwm
